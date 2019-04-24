5d2a54fd746ff11f9464cc45380c40db01955940
Pian Pawakapan <pp456@camembert.cs.cornell.edu>
2019-04-23 23:25:24 -0400

    adding save to option for eval

--- eval.py
+++ eval.py
@@ -20,7 +20,7 @@ sys.path.append('drnseg')
 sys.path.append('lib')
 
 parser = argparse.ArgumentParser(description='evaluation scheme')
-parser.add_argument('--modeltype', choices['psmnet_base'], default='psmnet_base')
+parser.add_argument('--modeltype', choices=['psmnet_base'], default='psmnet_base')
 parser.add_argument('--maxdisp', type=int, default=192,
                     help='maximum disparity')
 
@@ -30,6 +30,7 @@ parser.add_argument('--seqlength', type=int, default=3,
                     help='sequence length')
 parser.add_argument('--ckpt', default=None,
                     help='checkpoint model')
+parser.add_argument('--sample_output', action='store_true')
 args = parser.parse_args()
 
 # cuda
@@ -48,6 +49,23 @@ if use_cuda:
 if args.ckpt is not None:
     model.load_state_dict(torch.load(args.ckpt)['state_dict'])
 
+sh,sw = 256,512
+ch = torch.Tensor(range(sh)).unsqueeze(1).repeat(1,sw)
+cw = torch.Tensor(range(sw)).unsqueeze(0).repeat(sh,1)
+coord_matrix = torch.cat((cw.unsqueeze(-1),ch.unsqueeze(-1)),dim=-1)
+mult = torch.ones((sh,sw,2))
+mult[:,:,0] /= (sw-1)/2
+mult[:,:,1] /= (sh-1)/2
+
+if use_cuda:
+    coord_matrix,mult = coord_matrix.cuda(),mult.cuda()
+
+def get_grid(disp):
+    c = coord_matrix.view(1,sh,sw,2).repeat(disp.size(0),1,1,1)
+    c += torch.cat((disp.unsqueeze(-1),torch.zeros(disp.size(0),sh,sw,1).cuda()),dim=-1)
+    c = c*mult-1
+    return c
+
 def eval(dataloader): # only takes in supervised loader
 
     model.eval()
@@ -59,7 +77,7 @@ def eval(dataloader): # only takes in supervised loader
     len_iter = len(dataloader)
     d_iter = iter(dataloader)
     while iter_count < len_iter:
-
+        print(iter_count)
         img_L,img_R,y = next(d_iter)
         if use_cuda:
             img_L = img_L.cuda()
@@ -74,6 +92,13 @@ def eval(dataloader): # only takes in supervised loader
             with torch.no_grad():
                 output3 = model(img_L,img_R) # L-R input
             output3 = torch.squeeze(output3,1)
+
+            if args.sample_output:
+                coord3 = get_grid(output3)
+                warp3 = F.grid_sample(img_L,coord3,padding_mode="border")
+                imageio.imsave("sample_outputs/"+str(iter_count)+"_imgL.png",img_L[0].permute(1,2,0).detach().cpu().numpy())
+                imageio.imsave("sample_outputs/"+str(iter_count)+"_imgR.png",img_R[0].permute(1,2,0).detach().cpu().numpy())
+                imageio.imsave("sample_outputs/"+str(iter_count)+"_warped.png",warp3[0].permute(1,2,0).detach().cpu().numpy())
 
             s_loss = torch.mean((torch.abs(output3[mask]-y[mask])>3.0).float())*output3.size(0)
         
